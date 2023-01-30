ede2b4c80668d7a65ffa88ed81abda00d7d3e6f2
alexbalak21 <alex.balak@outlook.com>
2023-01-30 13:29:35 +0100

    Inital

---
+++ src/test/java/dev/alex/SpringDataJDBC/SpringDataJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.alex.SpringDataJDBC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
