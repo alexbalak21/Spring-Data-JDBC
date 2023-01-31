cf2cf07a896997d6be245d321840c03622eab967
alexbalak21 <alex.balak@outlook.com>
2023-01-31 14:31:01 +0100

    Solved NULL updated_on column.

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,12 +1,11 @@
 --post
 
-SET SQL_MODE='ALLOW_INVALID_DATES';
 CREATE TABLE Post(
 id INT auto_increment PRIMARY KEY,
 version INT,
 title VARCHAR(255) not null,
 content text not null,
 published_on TIMESTAMP not null,
-updated_on TIMESTAMP
+updated_on TIMESTAMP NULL DEFAULT NULL
 );
 
