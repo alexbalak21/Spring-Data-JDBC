1dabb5be83d42df7d41bd930f3450f7d5503106c
Alex Balak <alex.balak@outlook.com>
2025-10-24 15:21:03 +0200

    Removed unnecessary code

--- src/main/java/app/controller/UserController.java
+++ src/main/java/app/controller/UserController.java
@@ -5,18 +5,12 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import app.repository.UserRepository;
 import app.service.UserDetailsImpl;
+
+
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
-    
-    private final UserRepository userRepository;
-
-    public UserController(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
 
     @GetMapping
     public ResponseEntity<?> getUserDetails(@AuthenticationPrincipal UserDetailsImpl userDetails) {
